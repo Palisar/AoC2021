4a33874273f3146cc5e2de391bd82009c6c6d885
Palisar <palisar@hotmail.co.uk>
2021-12-10 20:36:25 +0000

    Working on Day_08

--- AdventOfCode/Day_08.cs
+++ AdventOfCode/Day_08.cs
@@ -3,32 +3,89 @@
 public class Day_08 : BaseDay
 {
 
-    private readonly string _input;
-    private readonly string[] _segments;
+    private readonly string[] _input;
 
     public Day_08()
     {
-         _input = File.ReadLines(InputFilePath).First();
-        _segments = _input.Split(new[] { '\n' });
+        _input = File.ReadAllLines(InputFilePath);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        return ValueTask.FromResult(Part1(_segments).ToString());
+        return ValueTask.FromResult(Part1(_input).ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return ValueTask.FromResult(Part2(_numbers).ToString());
+        return ValueTask.FromResult(Part2(_input).ToString());
     }
-    public static int Part1(string[] numbers)
+    public static int Part1(string[] segments)
     {
-        return 0;
+        var count = 0;
+        foreach (var item in segments)
+        {
+            var spliter = item.Split('|');
+            var output = spliter[1].Split(' ');
+            foreach (var digit in output)
+            {
+                int[] constantDigits = { 2, 4, 7, 3 };
+                if (constantDigits.Contains(digit.Length))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
-    public static int Part2(int[] numbers)
+    //    dddd
+    //   e    a
+    //   e    a
+    //    ffff
+    //   g    b
+    //   g    b
+    //    cccc
+    public static Dictionary<string, int> Digit = new Dictionary<string, int>
     {
-        return 0;
+        { "abcdefg", 8},
+        { "cdfbe" , 5},
+        { "acdfg" , 2},
+        { "abcdf" , 3},
+        { "abd" , 7},
+        { "abcdef" , 9},
+        { "bcdefg" , 6},
+        { "abef", 4},
+        { "abcdeg", 0},
+        { "ab", 1}
+    };
+
+    public static int Part2(string[] segments)
+    {
+        var result = 0;
+        foreach (var item in segments)
+        {
+            StringBuilder sb = new();
+            var spliter = item.Split('|');
+            var output = spliter[1].Split(' ');
+
+            foreach (var digit in output)
+            {
+                var ordered = String.Concat(digit.OrderBy(l => l));
+                if (Digit.ContainsKey(ordered))
+                {
+                    sb.Append(Digit[ordered].ToString());
+                }
+            }
+            result += int.Parse(sb.ToString());
+            sb.Clear();
+        }
+        return result;
+    }
+
+    public static Dictionary<string, int> Decypher(string input)
+    {
+
+        return new();
     }
 
 }
--- AoCTests/Day8Tests.cs
+++ AoCTests/Day8Tests.cs
@@ -6,11 +6,28 @@ public class Day8Tests
     public void Part1()
     {
         var data = new[] { "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce" };
+        var actual = Day_08.Part1(data);
+        actual.Should().Be(26);
     }
 
     [Fact]
     public void Part2()
     {
+        var data = new[] { "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc", "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea", "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef", "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce" };
+        var actual = Day_08.Part2(data);
+        actual.Should().Be(61229);
+    }
 
+    [Fact]
+    public void Trail()
+    {
+        var input= "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab";
+        var data = input.Split(' ').ToArray();
+        Dictionary<string, int> unscram = new();
+        foreach (var item in data)
+        {
+            String.Concat(item.OrderBy(x => x));
+            
+        }
     }
 }
